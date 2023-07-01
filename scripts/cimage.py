f68396cadd1ce7153b8e556fcc4c67bcdefbde45
waljan <48321741+waljan@users.noreply.github.com>
2023-07-01 19:15:44 +0200

    Update cimage.py

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -5,5 +5,5 @@ def convert_to_sd(img):
     shapes = []
     chunks = detect(img)
     for chunk in chunks:
-        shapes.append(chunk["score"] > 0.0)
+        shapes.append(chunk["score"] > 1.0)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
